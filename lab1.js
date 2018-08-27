cc1fe6ddc957925b8adae370c34aeeebb29936da
ywCN <wangyouhao90@gmail.com>
2018-08-27 18:46:44 -0400

    initial commit

---
+++ lab1.js
@@ -0,0 +1,25 @@
+const questionOne = function questionOne(arr) {
+  // Implement question 1 here
+};
+
+const questionTwo = function questionTwo(num) {
+  // Implement question 2 here
+};
+
+const questionThree = function questionThree(text) {
+  // Implement question 3 here
+};
+
+const questionFour = function questionFour(num) {
+  // Implement question 4 here
+};
+
+module.exports = {
+  firstName: 'YOUR FIRST NAME',
+  lastName: 'YOUR LAST NAME',
+  studentId: 'YOUR STUDENT ID',
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour
+};
