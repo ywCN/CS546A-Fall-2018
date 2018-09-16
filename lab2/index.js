015cfe6280984f07956303fcb85faa20774f155f
ywCN <wangyouhao90@gmail.com>
2018-09-16 16:24:19 -0400

    add comment for testing

--- lab2/index.js
+++ lab2/index.js
@@ -7,6 +7,7 @@ const obj4 = { a: 2, b: 4, c: { d: 1 } };
 const obj5 = { a: { b: { c: { d: 1 } } } };
 const obj6 = { a: { b: { c: { d: 1 } } } };
 const obj7 = { a: { b: { c: { d: 2 } } } };
+console.log('----------------testing deepEquality()----------------');
 console.log(deepEquality(obj1, obj2)); // true
 console.log(deepEquality(obj3, obj4)); // true
 console.log(deepEquality(obj5, obj6)); // true
