efd2045bd97aba0093ccb0f20b050ae443410281
ywCN <wangyouhao90@gmail.com>
2018-09-16 16:21:41 -0400

    update tests for deepEquality

--- lab2/index.js
+++ lab2/index.js
@@ -4,9 +4,11 @@ const obj1 = { a: 2, b: 3 };
 const obj2 = { b: 3, a: 2 };
 const obj3 = { c: { d: 1 }, a: 2, b: 4 };
 const obj4 = { a: 2, b: 4, c: { d: 1 } };
-const obj5 = { b: 2, a: 3, c: { d: 2 } };
+const obj5 = { a: { b: { c: { d: 1 } } } };
+const obj6 = { a: { b: { c: { d: 1 } } } };
+const obj7 = { a: { b: { c: { d: 2 } } } };
 console.log(deepEquality(obj1, obj2)); // true
-console.log(deepEquality(obj1, obj3)); // false
 console.log(deepEquality(obj3, obj4)); // true
-console.log(deepEquality(obj3, obj5)); // false
-console.log(deepEquality(obj4, obj5)); // false
+console.log(deepEquality(obj5, obj6)); // true
+console.log(deepEquality(obj1, obj5)); // false
+console.log(deepEquality(obj6, obj7)); // false
