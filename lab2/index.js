4ff6b64695086ef7fc1d6f820acadfe425b37025
ywCN <wangyouhao90@gmail.com>
2018-09-16 16:44:59 -0400

    better tests for deepEquality

--- lab2/index.js
+++ lab2/index.js
@@ -1,9 +1,13 @@
-const { deepEquality, uniqueElements } = require('./utilities');
+const {
+  deepEquality,
+  uniqueElements,
+  countOfEachCharacterInString
+} = require('./utilities');
 
 const obj1 = { a: 2, b: 3 };
 const obj2 = { b: 3, a: 2 };
-const obj3 = { c: { d: 1 }, a: 2, b: undefined };
-const obj4 = { a: 2, b: undefined, c: { d: 1 } };
+const obj3 = { c: { d: 1 }, a: { b: undefined } };
+const obj4 = { a: { b: undefined }, c: { d: 1 } };
 const obj5 = { a: { b: { c: { d: 1, e: 3 } } } };
 const obj6 = { a: { b: { c: { e: 3, d: 1 } } } };
 const obj7 = { a: { b: { c: { d: 1 } } } };
