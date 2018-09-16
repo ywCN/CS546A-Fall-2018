a52b887e32de0e6a99d4db366609e2ff46b97b14
ywCN <wangyouhao90@gmail.com>
2018-09-16 16:59:47 -0400

    skeleton of geometry.js

---
+++ lab2/geometry.js
@@ -0,0 +1,11 @@
+const volumeOfRectangularPrism = (length, width, height) => {};
+const surfaceAreaOfRectangularPrism = (length, width, height) => {};
+const volumeOfSphere = radius => {};
+const surfaceAreaOfSphere = radius => {};
+
+module.exports = {
+  volumeOfRectangularPrism,
+  surfaceAreaOfRectangularPrism,
+  volumeOfSphere,
+  surfaceAreaOfSphere
+};
