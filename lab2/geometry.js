ccad4979cb5d8647b0d9f129e7bde933a920f2b5
ywCN <wangyouhao90@gmail.com>
2018-09-16 17:08:16 -0400

    build type checking helper

--- lab2/geometry.js
+++ lab2/geometry.js
@@ -1,3 +1,19 @@
+const checkParams = params => {
+  for (const param of params) {
+    if (typeof param !== 'number') {
+      throw TypeError(`The param ${param} is not a number.`);
+    }
+    if (param <= 0) {
+      throw RangeError(`The param ${param} must be greater than 0.`);
+    }
+    if (param >= Number.MAX_SAFE_INTEGER) {
+      throw RangeError(
+        `The param ${param} must be smaller than ${Number.MAX_SAFE_INTEGER}.`
+      );
+    }
+  }
+};
+
 const volumeOfRectangularPrism = (length, width, height) => {};
 const surfaceAreaOfRectangularPrism = (length, width, height) => {};
 const volumeOfSphere = radius => {};
