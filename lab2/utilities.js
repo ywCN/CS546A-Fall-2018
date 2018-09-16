9f59805802fa5272f54e3a6a7d474f0eeb358603
ywCN <wangyouhao90@gmail.com>
2018-09-16 13:29:17 -0400

    debug deepEquality recursion

--- lab2/utilities.js
+++ lab2/utilities.js
@@ -12,6 +12,10 @@ const deepEquality = (obj1, obj2) => {
     const prop1 = obj1[key];
     const prop2 = obj2[key];
 
+    if (typeof prop1 !== typeof prop2) {
+      return false;
+    }
+
     if (
       typeof prop1 === 'object' &&
       typeof prop2 === 'object' &&
@@ -20,13 +24,26 @@ const deepEquality = (obj1, obj2) => {
       return false;
     }
 
-    if (prop1 !== prop2) {
+    if (
+      typeof prop1 !== 'object' &&
+      typeof prop2 !== 'object' &&
+      prop1 !== prop2
+    ) {
       return false;
     }
   }
 
   return true;
 };
+
+const t1 = { a: 2, b: 3 };
+const t2 = { b: 3, a: 2 };
+const t3 = { c: { d: 1 }, a: 2, b: 4 };
+const t4 = { a: 2, b: 4, c: { d: 1 } };
+const t5 = { b: 2, a: 3, c: { d: 2 } };
+console.log(deepEquality(t1, t2)); // true
+console.log(deepEquality(t3, t4)); // true
+console.log(deepEquality(t3, t5)); // false
 
 module.exports = {
   deepEquality
