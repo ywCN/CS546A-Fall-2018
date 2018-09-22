304ec140abc0ae5b102678b86ed34b495a646217
ywCN <wangyouhao90@gmail.com>
2018-09-22 13:48:10 -0400

    v2

--- lab3/app.js
+++ lab3/app.js
@@ -8,32 +8,35 @@ const { createMetrics } = require('./textMetrics');
 const fileNames = [
   {
     fileName: 'chapter1.txt',
-    fileResult: 'chapter1.result.json'
+    fileResultName: 'chapter1.result.json'
   },
   {
     fileName: 'chapter2.txt',
-    fileResult: 'chapter2.result.json'
+    fileResultName: 'chapter2.result.json'
   },
   {
     fileName: 'chapter3.txt',
-    fileResult: 'chapter3.result.json'
+    fileResultName: 'chapter3.result.json'
   }
 ];
 
 const main = async () => {
-  for (const { fileName, fileResult } of fileNames) {
+  for (const { fileName, fileResultName } of fileNames) {
     try {
-      const result = await readFile(fileResult, 'utf8');
+      const result = await readFile(fileResultName, 'utf8');
       console.log(result);
     } catch (e) {
       const fileStr = await getFileAsString(fileName);
-      const metric = createMetrics(fileStr);
 
-      fs.writeFile(fileResult, metric, e => {
-        if (e) {
-          throw e;
+      fs.writeFile(
+        fileResultName,
+        JSON.stringify(createMetrics(fileStr), null, 2),
+        e => {
+          if (e) {
+            throw e;
+          }
         }
-      });
+      );
     }
   }
 };
