12ec3bfbfd89cdee69be016bfca0c45670145943
ywCN <wangyouhao90@gmail.com>
2018-10-14 16:52:57 -0400

    about content

--- lab6/aboutRoute.js
+++ lab6/aboutRoute.js
@@ -3,12 +3,11 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
   try {
     res.json({
-      name: 'Your Name',
-      cwid: 'Your CWID',
-      biography:
-        '2 biography paragraphs separated by a new line character (\n).',
-      favoriteShows: ['array', 'of', 'favorite', 'shows'],
-      hobbies: ['array', 'of', 'hobbies']
+      name: 'Youhao Wang',
+      cwid: '10423006',
+      biography: 'Hi, my name is Youhao.\n I am doing homework for CS546.',
+      favoriteShows: ['The Good Place', 'The Wire', 'Lost', 'The Simpsons'],
+      hobbies: ['eat', 'sleep', 'coffee']
     });
   } catch (e) {
     res.status(500).send();
