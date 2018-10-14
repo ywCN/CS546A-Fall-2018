f9ea200c2a80fb9f7d5adc9ce8dedba10f904a88
ywCN <wangyouhao90@gmail.com>
2018-10-14 16:55:07 -0400

    story content

--- lab6/storyRoute.js
+++ lab6/storyRoute.js
@@ -3,8 +3,9 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
   try {
     res.json({
-      storyTitle: 'Story Title',
-      story: 'Your story.\nSimply use line breaks for paragraphs.\nLike this.'
+      storyTitle: 'Funny Story',
+      story:
+        'Hello, my name is Youhao.\n I am writting a story about github.\n I used git push -f and my teammates were angry.'
     });
   } catch (e) {
     res.status(500).send();
