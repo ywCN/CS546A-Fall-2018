ffc1c9b4b2b0a6b6e96ba5f0ff359b5d55805a58
ywCN <wangyouhao90@gmail.com>
2018-10-19 22:23:36 -0400

    revert

--- lab7/recipeRouter.js
+++ lab7/recipeRouter.js
@@ -14,7 +14,11 @@ router.get('/:id', async (req, res) => {
 
 // PUT Updates the specified recipe with by replacing the recipe with the new recipe content, and returns the updated recipe
 router.put('/:id', async (req, res) => {
-  validateAllFields(req.body);
+  try {
+    validateAllFields(req.body);
+  } catch (e) {
+    res.status(400).send('Input is not valid.');
+  }
   // NOTE: req.body needs to have all fields
   // use mutiple try catch blocks to do checking
   try {
@@ -33,7 +37,7 @@ const validateAllFields = obj => {
 
   for (const key of objKeys) {
     if (!expectedKeys.delete(key)) {
-      res.status(400).send('Input object is not valid.');
+      throw `The key ${key} of the input object ${obj} is not in the expect key set.`;
     }
   }
 
@@ -45,7 +49,7 @@ const validateAllFields = obj => {
     return;
   }
 
-  res.status(400).send('Input object is not valid.');
+  throw `The input object ${obj} is not valid.`;
 };
 
 // PATCH Updates the specified recipe with only the supplied changes, and returns the updated recipe
