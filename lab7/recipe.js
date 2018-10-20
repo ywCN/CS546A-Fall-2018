3e4560d305610e40dd423c7e2fac3c58cca85949
ywCN <wangyouhao90@gmail.com>
2018-10-19 23:21:06 -0400

    re-arrange for CRUD

--- lab7/recipe.js
+++ lab7/recipe.js
@@ -4,12 +4,6 @@ const uuidv4 = require('uuid/v4');
 // NOTE: no try catch in this file
 // TODO: build CRUD functions
 
-const getAllRecipes = async () => {
-  const recipes = await lab7recipes();
-  const allRecipes = await recipes.find({}).toArray();
-  return allRecipes;
-};
-
 const createRecipe = async data => {
   const recipes = await lab7recipes();
   const newRecipe = {
@@ -21,6 +15,12 @@ const createRecipe = async data => {
     throw 'Could not create new recipe.';
   }
   return newRecipe;
+};
+
+const getAllRecipes = async () => {
+  const recipes = await lab7recipes();
+  const allRecipes = await recipes.find({}).toArray();
+  return allRecipes;
 };
 
 module.exports = {
