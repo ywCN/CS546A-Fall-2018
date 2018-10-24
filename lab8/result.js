58b66c172a94a5db9c21a52594efebb7e7f9500f
ywCN <wangyouhao90@gmail.com>
2018-10-24 14:03:23 -0400

    improve syntax

--- lab8/result.js
+++ lab8/result.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
 
 router.post('/', (req, res) => {
-  res.render('pages/result', { isPal: isPalindrome(req.body.userInput) });
+  const isPal = isPalindrome(req.body.userInput);
+  res.render('pages/result', { isPal });
 });
 
 const isPalindrome = str => {
-  return false;
+  // const regex = new RegExp('[a-z]', 'i');
+  const validLetters = str
+    .toLowerCase()
+    .split('')
+    .filter(letter => /[a-z]/i.test(letter));
+  // const validLetters = [];
+
+  // for (const letter of str.toLowerCase()) {
+  //   if (regex.test(letter)) {
+  //     validLetters.push(letter);
+  //   }
+  // }
+
+  return validLetters.join() === validLetters.reverse().join();
 };
+
+isPalindrome('Go hang a salami, I’m a lasagna hog.');
 
 module.exports = router;
